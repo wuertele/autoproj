68491f4b865b45f387e7093820e741d4907e5000
David Wuertele <dave+github@wuertele.com>
2010-09-03 12:07:02 -0700

    add a second dynamic library

---
+++ libprojdyn2/proj_print.c
@@ -0,0 +1,17 @@
+/**
+***	proj_print.c		
+***	David Wuertele		Thu Sep  2 15:41:03 2010
+***	Steal This Program!!!
+**/
+
+/* headers for this library (always list first)  */
+#include <projdyn1.h>
+
+/* other headers */
+#include <commonstat.h>
+
+int
+proj_print (char * name)
+{
+   print_routine (name);
+}
