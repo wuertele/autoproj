e407a8d5278c6420c0093fb9a7ba06e16dcfe984
David Wuertele <dave+github@wuertele.com>
2010-09-03 11:54:29 -0700

    libtool supported

---
+++ include/projdyn1.h
@@ -0,0 +1,14 @@
+/**
+***	projdyn1.h		
+***
+***	David Wuertele		Thu Sep  2 16:04:29 2010
+***
+***	Steal This Program!!!
+**/
+
+#ifndef __PROJDYN1_SEEN__	/* Protect against re-inclusion of this file. */
+#define __PROJDYN1_SEEN__
+
+int proj_print (char * name);
+
+#endif /* __PROJDYN1_SEEN__ */
---
+++ libprojdyn1/proj_print.c
@@ -0,0 +1,14 @@
+/**
+***	proj_print.c		
+***	David Wuertele		Thu Sep  2 15:41:03 2010
+***	Steal This Program!!!
+**/
+
+#include <libcommonstat.h>
+#include <projdyn1.h>
+
+int
+proj_print (char * name)
+{
+   print_routine (name);
+}
